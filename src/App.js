b80dd4a5dd78d2b0288fc8706fed3c1cda77757b
ThiagoYgm <thyagosilvago@gmail.com>
2023-07-11 10:08:47 -0300

    arquivos finais

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Projects from './components/pages/Projects';
 import Container from './components/Layout/Container';
 import Footer from './components/Layout/Footer';
 import NavBar from './components/Layout/NavBar';
+import Project from './components/pages/Project';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route path="/projects" element={<Projects />}/>
           <Route path="/company" element={<Company />}/>
           <Route path="/newproject" element={<NewProject />}/>
+          <Route path="/project/:id" element={<Project />}/>
       </Routes>
       </Container>
       <Footer/>
